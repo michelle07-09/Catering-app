f57770c722e1d765b0987d8ca6ee71f6dfede174
MICHELLEHAMDANI07 <18223037@std.stei.itb.ac.id>
2026-01-01 22:41:26 +0700

    commit

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,8 +3,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import 'react-native-url-polyfill/auto';
 
 // IMPORTANT: Ganti dengan Supabase URL dan ANON KEY kamu
-const SUPABASE_URL = 'https://ewcnzlnierkpbakuikbc.supabase.co'; // Contoh: https://xxxxx.supabase.co
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3Y256bG5pZXJrcGJha3Vpa2JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4MTUyMTIsImV4cCI6MjA4MTM5MTIxMn0.WmESHsoqna5Wst1fLoTahJaxdbyhmKWr21s4YNWfjnY';
+const SUPABASE_URL = 'https://axpcluuaemzxrxsgdbef.supabase.co'; // Contoh: https://xxxxx.supabase.co
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4cGNsdXVhZW16eHJ4c2dkYmVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMTM4MzMsImV4cCI6MjA4MjU4OTgzM30.blgemvdow9Q3iUIowjavtO7mttb9Yl9QHZztj5wG8sE';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
